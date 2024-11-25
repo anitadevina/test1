5296cdc3dcd94a930b7ad50e9146bbdd140a2081
anita <anitadevina8041@gmail.com>
2024-11-25 21:31:58 +0700

    Initial commit

---
+++ connection.py
@@ -0,0 +1,22 @@
+{
+    "marketplace_prod": {
+
+        "host": "34.123.247.93",
+        "db": "marketplace",
+        "user": "postgres",
+        "password": "ftdebatch3",
+        "port": "5231"
+
+    },
+
+    "dwh": {
+
+        "host": "34.123.247.93",
+        "db": "dwh_devina",
+        "user": "postgres",
+        "password": "ftdebatch3",
+        "port": "5231"
+
+    }
+
+}
